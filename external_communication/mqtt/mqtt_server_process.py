1c3a7fa25f88f5cf089ca3312a7349a8f9be636a
kktai1512 <kktai1512@outlook.com>
2024-04-06 18:10:33 +0800

    delete action queue

--- external_communication/mqtt/mqtt_server_process.py
+++ external_communication/mqtt/mqtt_server_process.py
@@ -3,9 +3,9 @@ from logging import ERROR
 from external_communication.mqtt.mqtt_server import MqttServer
 from multiprocessing import Queue
 
-def mqtt_server_process(action_queue: Queue, incoming_from_mqtt_queue: Queue):
+def mqtt_server_process(incoming_from_mqtt_queue: Queue):
     try:
-        mqtt = MqttServer(action_queue, incoming_from_mqtt_queue)
+        mqtt = MqttServer(incoming_from_mqtt_queue)
         mqtt.initialise_server()
     except Exception as e:
         Logger.log(e, level=ERROR)
