1abeeb9e95eccd40f75d6b998cccbd2217879b11
kktai1512 <kktai1512@outlook.com>
2024-04-07 21:30:47 +0800

    lstm ai logic

---
+++ ai/ai_logic_backup.py
@@ -0,0 +1,28 @@
+from typing import List
+from pynq import Overlay, allocate
+import numpy as np
+
+class AILogic:
+    def __init__(self):
+        overlay = Overlay("/home/xilinx/external_comms/ai/design_1.bit")
+        self.dma = overlay.axi_dma_0
+        self.input_buffer = allocate(shape=(36,), dtype=np.float32)
+        self.output_buffer = allocate(shape=(1,), dtype=np.float32)
+    
+    def process(self, message: List[float]) -> str:
+        ####################### Start of AI logic ########################
+        ai_actions = ["ironMan", "hulk", "captAmerica", "shangChi", "bomb", "shield", "reload", "logout", "nothing"]
+        for index, num in enumerate(message): self.input_buffer[index] = num
+        
+        try:
+            self.dma.sendchannel.transfer(self.input_buffer)
+            self.dma.recvchannel.transfer(self.output_buffer)
+            self.dma.sendchannel.wait()
+            self.dma.recvchannel.wait()
+            result = ai_actions[int(self.output_buffer[0])]
+        except Exception as e:
+            print(f"ERROR is {e}")
+            result = ai_actions[-1]
+        print(f"THE RESULT IS {result}")
+        ####################### End of AI logic ##########################
+        return result
