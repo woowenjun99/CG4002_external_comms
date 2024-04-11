4d0511be02f40e35bbcbf8b339f739dc0d2eda83
woowenjun99 <woowenjun99@gmail.com>
2024-04-12 00:18:42 +0800

    Change to port 50053

--- external_communication/grpc/grpc_client.py
+++ external_communication/grpc/grpc_client.py
@@ -3,7 +3,7 @@ from pb.relay_node_pb2_grpc import RelayNodeStub
 
 class GrpcClient:
     def __init__(self) -> None:
-        channel = grpc.insecure_channel("127.0.0.1:50052")
+        channel = grpc.insecure_channel("127.0.0.1:50053")
         self.stub = RelayNodeStub(channel)
 
     def update_game_state(self, message):
