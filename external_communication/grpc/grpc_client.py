86b04abaa062488a37a5d63a3fbeb9b436b09a99
Woo Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2024-04-12 14:38:48 +0800

    Fix connection issue (#2)
    * Add in handshake
    
    * Rename to predicted game state
    
    * Change to port 50053

--- external_communication/grpc/grpc_client.py
+++ external_communication/grpc/grpc_client.py
@@ -3,8 +3,8 @@ from pb.relay_node_pb2_grpc import RelayNodeStub
 
 class GrpcClient:
     def __init__(self) -> None:
-        channel = grpc.insecure_channel("127.0.0.1:50052")
+        channel = grpc.insecure_channel("127.0.0.1:50053")
         self.stub = RelayNodeStub(channel)
 
     def update_game_state(self, message):
-        self.stub.processGameState(message)
+        self.stub.processGameState(message, wait_for_ready=True)
