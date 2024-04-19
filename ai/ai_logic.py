10384ef86b979f9c099769b4c242adbbf03a4135
kktai1512 <kktai1512@outlook.com>
2024-04-19 23:48:18 +0800

    initial commit

--- ai/ai_logic.py
+++ ai/ai_logic.py
@@ -1,68 +1,28 @@
 from typing import List
-
+from pynq import Overlay, allocate
 import numpy as np
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
-# Define the model
-class LSTMModel(nn.Module):
-    def __init__(self, num_classes):
-        super(LSTMModel, self).__init__()
-        self.lstm1 = nn.LSTM(input_size=6, hidden_size=64, batch_first=True)
-        self.dropout1 = nn.Dropout(0.5)
-        self.lstm2 = nn.LSTM(input_size=64, hidden_size=32, batch_first=True)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc = nn.Linear(32, num_classes)
-
-    def forward(self, x):
-        # LSTM with input (batch, seq, feature) when batch_first=True
-        x, _ = self.lstm1(x)  # output from first LSTM layer
-        x = self.dropout1(x)
-        x, _ = self.lstm2(x)  # output from second LSTM layer
-        x = self.dropout2(x)
-        x = x[:, -1, :]  # Select the last time step output for feeding into the fully connected layer
-        out = self.fc(x)
-        return out
-
-def predict(model, sample):
-    model.eval()
-    sample_tensor = torch.tensor(sample, dtype=torch.float).unsqueeze(0)
-    with torch.no_grad():
-        sample_tensor = sample_tensor.to(device)
-        output = model(sample_tensor)
-        
-        _, predicted_class = torch.max(output, dim=1)
-        return predicted_class.item()
-    
-
-mappings ={'0': 'shield',
-    '1': 'bomb',
-    '2': 'hulk',
-    '3': 'logout',
-    '4': 'reload',
-    '5': 'shangChi',
-    '6': 'captAmerica',
-    '7': 'ironMan',
-    '8': 'nothing'}
-
-device = torch.device("cpu")
 
 class AILogic:
     def __init__(self):
-        self.model = LSTMModel(num_classes=9)
-        # self.model.load_state_dict(torch.load('/home/xilinx/external_comms/ai/lstm_model_94.pth'))
-        self.model.load_state_dict(torch.load('/home/xilinx/external_comms/ai/lstm_FINAL_state.pth'))
-        self.model.to(device)
-        self.model.eval()
-
+        overlay = Overlay("/home/xilinx/external_comms/ai/design_1.bit")
+        self.dma = overlay.axi_dma_0
+        self.input_buffer = allocate(shape=(36,), dtype=np.float32)
+        self.output_buffer = allocate(shape=(1,), dtype=np.float32)
+    
     def process(self, message: List[float]) -> str:
-            sample = message
-            final = [sample[i:i + 6] for i in range(0, len(sample), 6)]
-            class_index = predict(self.model, final)
-            # print(class_index)
-            # print("Prediction is " + mappings[str(class_index)])
-            action_name = mappings[str(class_index)]
-
-            return action_name
+        ####################### Start of AI logic ########################
+        ai_actions = ["ironMan", "hulk", "captAmerica", "shangChi", "bomb", "shield", "reload", "logout", "nothing"]
+        for index, num in enumerate(message): self.input_buffer[index] = num
+        
+        try:
+            self.dma.sendchannel.transfer(self.input_buffer)
+            self.dma.recvchannel.transfer(self.output_buffer)
+            self.dma.sendchannel.wait()
+            self.dma.recvchannel.wait()
+            result = ai_actions[int(self.output_buffer[0])]
+        except Exception as e:
+            print(f"ERROR is {e}")
+            result = ai_actions[-1]
+        print(f"THE RESULT IS {result}")
+        ####################### End of AI logic ##########################
+        return result
